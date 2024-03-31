689749d639d22c19123c0a1c87bda155beaf453e
h1nekora <93097793+h1nekora@users.noreply.github.com>
2024-03-31 19:41:25 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -8,63 +8,63 @@ Automata::Automata() {
     cash = 0;
     state = OFF;
     menu = { "Espresso",
-	    "Cappuccino",
-	    "Latte",
-	    "Cacao",
-	    "Hot chocolate",
-	    "Americano",
-	    "Black tea",
-	    "Green tea" };
+	"Cappuccino",
+	"Latte",
+	"Cacao",
+	"Hot chocolate",
+	"Americano",
+	"Black tea",
+	"Green tea" };
     prices = { 100, 150, 180, 50, 75, 110, 80, 80 };
 }
 Automata::~Automata() {}
 void Automata::on() {
     if (state == OFF) {
-	state = WAIT;
+    state = WAIT;
     }
 }
 void Automata::off() {
     if (state == WAIT) {
-	state = OFF;
+    state = OFF;
     }
 }
 void Automata::coin(int money) {
     if (state == WAIT || state == ACCEPT) {
-	state = ACCEPT;
-	cash += money;
+    state = ACCEPT;
+    cash += money;
     }
 }
 void Automata::getMenu() {
     if (state == WAIT || state == ACCEPT) {
-	std::cout << "MENU: ";
-	for (int index = 0; index < menu.size();index++) {
-		std::cout << index+1 << ") " << menu[index] << " " << prices[index] << " | ";
-	}
-	std::cout<< std::endl;
-	}
+    std::cout << "MENU: ";
+    for (int index = 0; index < menu.size(); index++) {
+        std::cout << index+1 << ") " << menu[index] << " " << prices[index] << " | ";
+    }
+    std::cout<< std::endl;
+    }
 }
 STATES Automata::getState() {
     return state;
 }
 void Automata::choice(int variant) {
-   if (state == ACCEPT && variant >= 1 && variant <= menu.size()) {
-	state = CHECK;
-	check(variant);
-   }
+    if (state == ACCEPT && variant >= 1 && variant <= menu.size()) {
+    state = CHECK;
+    check(variant);
+    }
 }
 void Automata::check(int variant) {
     if (cash >= prices[variant - 1]) {
-	cash -= prices[variant - 1];
-	cook();
+    cash -= prices[variant - 1];
+    cook();
     }
     else {
-	state = ACCEPT;
+    state = ACCEPT;
     }
 }
 void Automata::cancel() {
     if (state == ACCEPT || state == CHECK) {
-	cash = 0;
-	state = WAIT;
+    cash = 0;
+    state = WAIT;
     }
 }
 void Automata::cook() {
